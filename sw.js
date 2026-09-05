64775efb15a9e45b39c2d65bb0615c4f61f2d2b6
jackalq <jackalqkimo@yahoo.com.tw>
2026-09-05 14:26:01 +0800

    Refresh PWA cache for melee and counter damage update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_NAME='gundam-math-v13-sprite-volley';
+const CACHE_NAME='gundam-math-v14-melee-counter-quarter';
 const APP_SHELL=[
   './','./index.html','./styles.css',
   './css-base-layout.css','./css-duel-hero.css','./css-duel-enemy.css','./css-mech-detail.css','./css-question-review.css','./css-war-layout.css','./css-war-units.css','./css-war-shared-mechs.css','./css-ui.css','./css-sprite-mechs.css','./css-sprite-hotfix.css','./weapon-effects.css','./weapon-sprites.svg',
